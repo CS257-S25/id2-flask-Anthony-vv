83dcd25b08aa7824077206b022e806094d6f76b7
Anthony Vazquez - Vazquez <vazqueza@carleton.edu>
2025-05-06 17:12:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
+"""Flask application for comparing COVID-19 statistics for multiple countries on a given date."""
+
 from flask import Flask
 from ProductionCode import covid_stats
 
 app = Flask(__name__)
-"""This is the main text for the Flask application, it wil provide the user information for formatting and an example"""
+
 @app.route('/')
 def homepage():
+    """Display instructions for how to use the COVID-19 comparison endpoint."""
     return (
         "Welcome to my ID2 Application!\n\n"
         "Here you can compare COVID-19 stats for multiple countries using one date:\n"
         "Please use this format:\n"
-        "/compare/date/ country1, country2.\n\n"
+        "/compare/date/country1,country2\n\n"
         "The date should be in the format YYYY-MM-DD.\n"
         "\nExample:\n"
         "/compare/2020-04-19/US,GB"
     )
-    """"def compare_invalid_date(self):"""
+
 @app.route('/compare/<date>/<countries>')
 def compare(date, countries):
-    """This function compares COVID-19 stats for multiple countries using one date"""
+    """Compare COVID-19 stats for the given countries on the specified date."""
     try:
         output = f"COVID-19 data for {date}:\n"
         for country in countries.split(','):
